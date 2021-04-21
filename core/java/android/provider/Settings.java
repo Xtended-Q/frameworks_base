deee815cdb1ee9b2fae3045c31d6223cd71c6c41
spkal01 <kalligeross@gmail.com>
2021-04-21 18:51:56 +0530

    Make use of app icon in NotificationHeaderViewWrapper togglable [1/2]
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>
    Change-Id: Ifd7a32bfefa30aad66a3a512d7b3abd7d9aa1397

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7302,6 +7302,12 @@ public final class Settings {
         public static final String STATUSBAR_ICONS_STYLE = "statusbar_icons_style";
 
         /**
+         * Whether should the icons on notification headers have the app color
+         * @hide
+         */
+        public static final String HEADER_ICONS_STYLE = "headers_icons_style";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -7546,6 +7552,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SYSTEMUI_PLUGIN_VOLUME);
             PRIVATE_SETTINGS.add(BRIGHTNESS_SLIDER_STYLE);
             PRIVATE_SETTINGS.add(STATUSBAR_ICONS_STYLE);
+            PRIVATE_SETTINGS.add(HEADER_ICONS_STYLE);
         }
 
         /**
