0d6825b2f33b3da6fb102410292ab9a885f4accc
Jeff DeCew <jeffdq@google.com>
2021-10-21 19:48:19 +0000

    Allow forcing status bar state changes and do so when the screen turns off.
    This adds a force flag, which we will use when turning the screen off to make sure that all UI components are reset to the SHADE state regardless.
    
    Bug: 189575031
    Test: make a call; lock screen; pull down shade
    Merged-In: I79baeb71ac5d1ed45602ac55cdca996b3bed0ac3
    Change-Id: I79baeb71ac5d1ed45602ac55cdca996b3bed0ac3
    (cherry picked from commit 921d95582244cec394cc701eb5fc17e97502584d)
    Merged-In:I79baeb71ac5d1ed45602ac55cdca996b3bed0ac3
    Signed-off-by: Ray <vincentrayananda@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3855,6 +3855,10 @@ public class StatusBar extends SystemUI implements DemoMode,
     }
 
     private boolean updateIsKeyguard() {
+        return updateIsKeyguard(false /* force */);
+    }
+
+    private boolean updateIsKeyguard(boolean force) {
         updateBlurVisibility();
         boolean wakeAndUnlocking = mBiometricUnlockController.getMode()
                 == BiometricUnlockController.MODE_WAKE_AND_UNLOCK;
@@ -3884,7 +3888,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 showKeyguardImpl();
             }
         } else {
-            return hideKeyguardImpl();
+            return hideKeyguardImpl(force);
         }
         return false;
     }
@@ -4026,11 +4030,11 @@ public class StatusBar extends SystemUI implements DemoMode,
     /**
      * @return true if we would like to stay in the shade, false if it should go away entirely
      */
-    public boolean hideKeyguardImpl() {
+    public boolean hideKeyguardImpl(boolean force) {
         mIsKeyguard = false;
         Trace.beginSection("StatusBar#hideKeyguard");
         boolean staying = mStatusBarStateController.leaveOpenOnKeyguardHide();
-        if (!(mStatusBarStateController.setState(StatusBarState.SHADE))) {
+        if (!(mStatusBarStateController.setState(StatusBarState.SHADE, force))) {
             //TODO: StatusBarStateController should probably know about hiding the keyguard and
             // notify listeners.
 
@@ -4651,7 +4655,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                 // is correct.
                 mHandler.post(() -> onCameraLaunchGestureDetected(mLastCameraLaunchSource));
             }
-            updateIsKeyguard();
+            // When finished going to sleep, force the status bar state to avoid stale state.
+            updateIsKeyguard(true /* force */);
         }
 
         @Override
